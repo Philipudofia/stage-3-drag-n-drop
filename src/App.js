7dafe14c947147f1a017e3b5e671435edb9c705c
Philipudofia <philipudofia2004@gmail.com>
2023-09-20 15:07:43 +0100

    search left

--- src/App.js
+++ src/App.js
@@ -14,83 +14,101 @@ import Images8 from './Images/9.jpg'
 import Images9 from './Images/10.jpg'
 import Images10 from './Images/11.jpg'
 import Images11 from './Images/12.jpg'
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { SortableContext, arrayMove, useSortable, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 function App() {
   const [signedIn, setSignedIn] = React.useState(false)
-  const [query, setQuery] = React.useState('')
+  const [query, setQuery] = React.useState()
   const [imageItem, setImageItem] = React.useState([
     {
+        "id":0,
         "img":Images0,
         "tag":'Dice'
     },
     {
+        "id":1,
         "img":Images1,
         "tag":'Apple'
     },
     {
+        "id":2,
         "img":Images2,
         "tag":'Tree House'
     },
     {
+        "id":3,
         "img":Images3,
         "tag":'Night Tree'
     },
     {
+        "id":4,
         "img":Images4,
         "tag":'Bush'
     },
     {
+        "id":5,
         "img":Images5,
         "tag":'Wood Man'
     },
     {
+        "id":6,
         "img":Images6,
         "tag":'Holding Bulb'
     },
     {
+        "id":7,
         "img":Images7,
         "tag":'Young Boy'
     },
     {
+        "id":8,
         "img":Images8,
         "tag":'Brain Socket'
     },
     {
+        "id":9,
         "img":Images9,
         "tag":'Flower on Table'
     },
     {
+        "id":10,
         "img":Images10,
         "tag":'Sky'
     },
     {
+        "id":11,
         "img":Images11,
         "tag":'Dodge'
     }
 ])
-React.useState(()=>{
-  setImageItem( imageItem.filter((e)=> e.tag.includes(query)))
-},[query])
-// drag and drop
-let todoItemDrag = useRef()
-let todoItemDragOver = useRef()
-
-function onDragStart(e,index){
-todoItemDrag.current = index
-}  
-function onDragEnter(e,index){
-todoItemDragOver.current = index
-}  
-function onDragEnd(e, index){
-const arr1 = [...imageItem]
-const itemMain = arr1[todoItemDrag.current]
-arr1.splice(todoItemDrag.current, 1)
-arr1.splice(todoItemDragOver.current, 0, itemMain)
-
-todoItemDrag.current= null;
-todoItemDragOver.current = null;
-setImageItem(arr1)
-}  
+const SortableUser = ({ item }) =>{
+  const {attributes,listeners,setNodeRef,transform,transition} = useSortable({ id: item.id });
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform)
+  }
+  return(
+    <div ref={setNodeRef} style={style} {...attributes}{...listeners} className='cardCover'>
+        <img
+          src={item.img} alt="" 
+        />
+        <p>{item.tag}</p>
+    </div>
+  )
+}
+const onDragEnd = (e) =>{
+    const { active,over } = e;
+    if (active.id === over.id) {
+      return
+    }
+    setImageItem((items) => {
+      const oldIndex = items.findIndex((user) => user.id === active.id)
+      const newIndex = items.findIndex((user) => user.id === over.id)
+      return arrayMove(items, oldIndex, newIndex)
+    })
+  }
   return (
     <div>
       {signedIn ? <React.Fragment>
@@ -99,31 +117,19 @@ setImageItem(arr1)
           setQuery ={e=> setQuery(e)}
         />
         <Home 
-          cards = {query ? imageItem.filter((e)=> {return e.tag.includes(query)}).map((e,index)=> {
-            return <div className='cardCover'                         
-            draggable 
-            onDragStart={(e)=>onDragStart(e,index)}
-            onDragEnter={(e)=>onDragEnter(e,index)}
-            onDragEnd={(e)=>onDragEnd(e,index)}>
-                <img
-                key={index} 
-                src={e.img} alt="" 
-                />
-                <p>{e.tag}</p>
-            </div>
-          }) :imageItem.map((item,index)=> {
-            return <div className='cardCover'                         
-            draggable 
-            onDragStart={(e)=>onDragStart(e,index)}
-            onDragEnter={(e)=>onDragEnter(e,index)}
-            onDragEnd={(e)=>onDragEnd(e,index)}>
-                <img
-                key={index} 
-                src={item.img} alt="" 
-                />
-                <p>{item.tag}</p>
-            </div>
-          }) }
+          cards = {
+              <>
+                <DndContext
+                      collisionDetection={closestCenter}
+                      onDragEnd={onDragEnd}>
+                      <SortableContext items={imageItem} >
+                          {imageItem.map((item, index) =>{
+                            return <SortableUser key={index} item={item} />
+                          })}
+                      </SortableContext>
+                    </DndContext>
+              </>
+              }
         />
       </React.Fragment> : ''}
       {signedIn ? '': <SignIn
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,6 @@
 import React, { useRef } from 'react';
 import './Home.css';
 import ClipLoader from "react-spinners/ClipLoader";
-// import Images0 from './Images/1.jpg'
-// import Images1 from './Images/2.jpg'
-// import Images2 from './Images/3.jpg'
-// import Images3 from './Images/4.jpg'
-// import Images4 from './Images/5.jpg'
-// import Images5 from './Images/6.jpg'
-// import Images6 from './Images/7.jpg'
-// import Images7 from './Images/8.jpg'
-// import Images8 from './Images/9.jpg'
-// import Images9 from './Images/10.jpg'
-// import Images10 from './Images/11.jpg'
-// import Images11 from './Images/12.jpg'
 
 export default function Home(props) {
     const [loading, setLoading] = React.useState(true)
@@ -28,75 +16,6 @@ export default function Home(props) {
         borderColor: "red",
       };
       let color= "rgb(176, 205, 10)";
-    // const [imageItem, setImageItem] = React.useState([
-    //         {
-    //             "img":Images0,
-    //             "tag":'Dice'
-    //         },
-    //         {
-    //             "img":Images1,
-    //             "tag":'Apple'
-    //         },
-    //         {
-    //             "img":Images2,
-    //             "tag":'Tree House'
-    //         },
-    //         {
-    //             "img":Images3,
-    //             "tag":'Night Tree'
-    //         },
-    //         {
-    //             "img":Images4,
-    //             "tag":'Bush'
-    //         },
-    //         {
-    //             "img":Images5,
-    //             "tag":'Wood Man'
-    //         },
-    //         {
-    //             "img":Images6,
-    //             "tag":'Holding Bulb'
-    //         },
-    //         {
-    //             "img":Images7,
-    //             "tag":'Young Boy'
-    //         },
-    //         {
-    //             "img":Images8,
-    //             "tag":'Brain Socket'
-    //         },
-    //         {
-    //             "img":Images9,
-    //             "tag":'Flower on Table'
-    //         },
-    //         {
-    //             "img":Images10,
-    //             "tag":'Sky'
-    //         },
-    //         {
-    //             "img":Images11,
-    //             "tag":'Dodge'
-    //         }
-    // ])
-    // let todoItemDrag = useRef()
-    // let todoItemDragOver = useRef()
-    
-    // function onDragStart(e,index){
-    //     todoItemDrag.current = index
-    // }  
-    // function onDragEnter(e,index){
-    //     todoItemDragOver.current = index
-    // }  
-    // function onDragEnd(e, index){
-    //     const arr1 = [...imageItem]
-    //     const itemMain = arr1[todoItemDrag.current]
-    //     arr1.splice(todoItemDrag.current, 1)
-    //     arr1.splice(todoItemDragOver.current, 0, itemMain)
-
-    //     todoItemDrag.current= null;
-    //     todoItemDragOver.current = null;
-    //     setImageItem(arr1)
-    // }  
 
     return(
         <div>
@@ -110,19 +29,6 @@ export default function Home(props) {
                 />:<div className="gallery">
                 <div className="galleryContainer">
                     <div className="cards">
-                        {/* {imageItem.map((item,index)=> {
-                            return <div className='cardCover'                         
-                            draggable 
-                            onDragStart={(e)=>onDragStart(e,index)}
-                            onDragEnter={(e)=>onDragEnter(e,index)}
-                            onDragEnd={(e)=>onDragEnd(e,index)}>
-                                <img
-                                key={index} 
-                                src={item.img} alt="" 
-                                />
-                                <p>{item.tag}</p>
-                            </div>
-                        })} */}
                         {props.cards}
                     </div>
                 </div>
